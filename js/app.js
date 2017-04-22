7279a73e41aeb396bb9183d722a61cf75a301195
Bender85 <nikitosik85@gmail.com>
2017-04-22 18:44:34 +0300

    add sound sample and create script

--- js/app.js
+++ js/app.js
@@ -4,4 +4,28 @@
 (function() {
     'use strict';
     console.log('ready');
+
+    function playSound(e) {
+        // console.log(e.keyCode);
+        // const audio = document.querySelector('audio[data-key="'+ e.keyCode +'"]'); // old
+        const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`); // es6
+        const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+        // console.log(audio);
+        // console.log(key);
+        if(!audio) return; // stop the function
+        audio.currentTime = 0 // rewind to the start
+        audio.play();
+        key.classList.add('playing');
+    }
+
+    function removeTransition(e) {
+        if(e.propertyName !== 'transform') return; // skip it is a not transform
+        this.classList.remove('playing');
+    }
+
+    const keys = document.querySelectorAll('.key');
+    keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+
+    window.addEventListener('keydown', playSound);
+
 })();
